1d7e49cedbfef61a1130f50994897e09706d57c1
1060905996 <liu1060905996*>
2020-10-21 19:51:20 +0800

    commit java demo

--- src/main/java/com/imooc/chapter02/obj5/AnimalTest.java
+++ src/main/java/com/imooc/chapter02/obj5/AnimalTest.java
@@ -1,4 +1,13 @@
 package com.imooc.chapter02.obj5;
 
 public class AnimalTest {
+    public static void main(String[] args) {
+        Animal one=new Animal("tutu1",2);
+        Animal two=new Animal("tutu1",2);
+
+        System.out.println(one.equals(two));
+    }
+
+
+
 }
